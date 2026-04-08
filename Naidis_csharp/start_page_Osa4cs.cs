5e5a6b728799e9c918ab4c9e48c109a4adda1d2a
domdom <dominic.kekkonen@gmail.com>
2026-04-08 12:42:32 +0300

    Refactor main menu logic in start_page_Osa4cs

--- Naidis_csharp/start_page_Osa4cs.cs
+++ Naidis_csharp/start_page_Osa4cs.cs
@@ -1,9 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Naidis_csharp
 {
     internal class start_page_Osa4cs
@@ -11,41 +5,46 @@ namespace Naidis_csharp
     {
         public static void Main(string[] args)
         {
-
-            while (true)
+            Console.WriteLine("4. Osa Funktsioonid\n========================");
+            Console.WriteLine("1. Faili kirjutamine\n2. Faili lugemine\n3. Ridade lugemine\n4. Ridade lugemine listiks\n5. Listi muutmine\n6. Otsing nimekirjas\n7. Listi salvestamine\n0. Välja");
+            int valik = int.Parse(Console.ReadLine());
+            string fail = "";
+            List<string> list = new List<string>();
+            switch (valik)
             {
-                Console.WriteLine("4. Osa Funktsioonid");
-                string valik = Console.ReadLine();
-                string fail = "";
-                List<string> list= new List<string>();
-                switch (valik)
-                {
-                    case "1": Osa4_funktsioonid.Faili_kirjutamine(); break;
-                    case "2": Osa4_funktsioonid.Faili_lugemine(); break;
-                       
-                    case "3":
-                        Console.Write("failinimi: ");
-                        fail = Console.ReadLine();
-                        Osa4_funktsioonid.Ridade_lugemine(fail);
-                  
+                case 0:
+                    Console.Clear();
+                    break;
+                case 1:
+                    Osa4_funktsioonid.Faili_kirjutamine();
+                    break;
+                case 2:
+                    Osa4_funktsioonid.Faili_lugemine();
+                    break;
 
-                        break;
-                    case "4":
-                        Console.Write("failinimi: ");
-                        fail = Console.ReadLine();
-                        list =  Osa4_funktsioonid.Ridade_lugemine_listiks(fail);
-                        break;
+                case 3:
+                    Console.Write("Sisesta failinimi: ");
+                    fail = Console.ReadLine();
+                    Osa4_funktsioonid.Ridade_lugemine(fail);
+                    break;
+                case 4:
+                    Console.Write("Sisesta failinimi: ");
+                    fail = Console.ReadLine();
+                    Osa4_funktsioonid.Ridade_lugemine_listiks(fail);
+                    break;
 
-                    case "5": Osa4_funktsioonid.Listi_muutmine_kuvamine(); break;
-                    case "6": Osa4_funktsioonid.Otsing_nimekirjast(); break;
-                    case "7":Osa4_funktsioonid.Listi_salvestamine(); break;
-                        
-
-
-
-                        break;
-                    default: Console.Write("sisesta kehtiv number: "); break;
-                }
+                case 5:
+                    Osa4_funktsioonid.Listi_muutmine_kuvamine();
+                    break;
+                case 6:
+                    Osa4_funktsioonid.Otsing_nimekirjast();
+                    break;
+                case 7:
+                    Osa4_funktsioonid.Listi_salvestamine();
+                    break;
+                default:
+                    Console.Write("sisesta kehtiv number (1-7): ");
+                    break;
             }
         }
     }
